64ae4569d1b993a44f97b824c1ee7b59cf1e9305
VashuGarg <VashuGarg466@gmail.com>
2021-04-18 21:01:37 +0530

    initial

---
+++ DevopsSpringGitAssignment/src/test/java/com/hcl/Devops/Graded/DevopsSpringGitAssignment/DevopsSpringGitAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.Devops.Graded.DevopsSpringGitAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsSpringGitAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
